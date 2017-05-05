91959abef661118f7c4f2fba23cfe6cbdeb40a3c
Blazejek <Blazejek@BLAZEJEK>
2017-05-05 18:12:09 +0200

    commented out couts,

--- GA_TravellingSalesman/GA_TravellingSalesman.cpp
+++ GA_TravellingSalesman/GA_TravellingSalesman.cpp
@@ -3,12 +3,28 @@
 #include "Individual.h"
 #include "PathFinder.h"
 #include <iostream>
+#include <random>
 #include "stdafx.h"
 
 
 int main()
 {
-	int test[10][10] = { { 47,32,70,14,95,79,46,4,13,29 },
+	int test[100][100];
+	std::uniform_int_distribution<int> distMap(1, 45);
+	std::mt19937 generator;
+
+	for (std::size_t i = 0; i < 100; ++i)
+	{
+		for (std::size_t j = 0; j < 100; ++j)
+		{
+			test[j][i] = distMap(generator);
+			if (j == i)
+				test[j][i] = 0;
+		}
+	};
+
+	int test2[10][10]= { 
+	{ 47,32,70,14,95,79,46,4,13,29 },
 	{ 32,54,57,13,100,28,80,25,25,87 },
 	{ 37,67,64,51,28,13,77,21,63,31 },
 	{ 1,62,87,23,41,79,93,58,85,82 },
@@ -18,25 +34,32 @@ int main()
 	{ 69,42,32,99,50,90,17,13,22,68 },
 	{ 19,77,67,73,37,99,66,16,20,50 },
 	{ 40,14,57,31,8,77,56,30,37,19 } };
-	Individual map(10, 10);
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	test[i % 10] = 0;
-	//	map.fillRow(test, 10, i);
-	//	test[i % 10] = 3;
-	//}
-	for (int i = 0; i < 10; i++)
-		map.fillRow(test[i], 10, i);
-
-
+	Individual map(100, 100);
+	
+	for (int i = 0; i < 100; i++)
+		map.fillRow(test[i], 100, i);
+	
 
 	std::cout << "map is ready " << map.getSize1() << "x" << map.getSize2() << std::endl;
-	std::vector<std::size_t> v = { 0, 1, 2, 3,4,5,6 };
-	PathFinder solution(map,v ,15);
-	solution.initRandDevEng();
-	std::cout << "finding solution" << std::endl;
-	solution.findSolution(10, 100, 10);
+	std::vector<std::size_t> v;
+	v.resize(80);
+	std::size_t i = 0;
+	for (auto it = v.begin(); it != v.end(); ++it)
+		*it = i++;
+	std::cout << "locations to visit\n";
+	for (auto it = v.begin(); it != v.end(); ++it)
+		std::cout << *it << " ";
 
+	std::cout << "creating solution obj\n";
+	PathFinder solution(map,v ,100);
+	
+
+	std::cout << "finding solution" << std::endl;
+	Path bestPath=solution.findSolution(10, 100, 10);
+	
+	std::cout << "Best path\n";
+	for (auto it = bestPath.pathPoints.begin(); it != bestPath.pathPoints.end(); ++it)
+		std::cout << *it << " ";
 
 
 
